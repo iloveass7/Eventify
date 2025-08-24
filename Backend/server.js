b9658974f7845dcd7ff7c2fb6cc7219fdfb08b28
Syed Abir Hossain <120031628+iloveass7@users.noreply.github.com>
2025-08-24 22:48:06 +0600

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -19,7 +19,7 @@ export const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://eventify-pi-five.vercel.app/",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   })
